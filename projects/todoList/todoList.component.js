e8b9a86f1ed06c5c4a47159bda27afab951a9f10
Anton Parayno <anton.parayno@gmail.com>
2017-11-13 12:17:43 -0500

    added focus directive

--- projects/todoList/todoList.component.js
+++ projects/todoList/todoList.component.js
@@ -1,88 +1,108 @@
-'use strict'
+(function() {
+    'use strict'
 
-angular.
-  module('todoList.project').
-    component('todoList', {
-    	templateUrl : 'projects/todoList/todoList.template.html',
-    	controller: function TodoController() {
-    	  var self = this;
+    angular.
+      module('todoList.project').
+        component('todoList', {
+        	templateUrl : 'projects/todoList/todoList.template.html',
+        	controller: TodoController
+        });
 
-    	  // create a unique id for the todo
 
-		  self.uuid = function () {
-			/*jshint bitwise:false */
-			var i, random;
-			var uuid = '';
+    // component controller function
+    function TodoController() {
+              var self = this;
 
-			for (i = 0; i < 32; i++) {
-				random = Math.random() * 16 | 0;
-				if (i === 8 || i === 12 || i === 16 || i === 20) {
-					uuid += '-';
-				}
-				uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
-			}
+              // storage for the todos
+              self.todoList = [];
+              // this will keep the value of the input field blank
+              self.todo = '';
+              // used to set all, active, completed tabs
+              self.tab = 1;
+              self.tabIsSet = tabIsSet;
+              self.setTab = setTab;
+              // bindable functions
+              self.uuid = uuid;
+              self.addTodo = addTodo;
+              self.toggleTodo = toggleTodo;
+              self.toggleAll = toggleAll;
+              self.editTodo = editTodo;
+              self.deleteTodo = deleteTodo;
+              self.clearCompleted = clearCompleted;
 
-			return uuid;
-		  }	
 
-          // storage for the todos
-    	  self.todoList = [];
-          // this will keep the value of the input field blank
-          self.todo = '';
 
-    	  self.addTodo = function (todo) { 
-            if(todo) {
-    	  	  self.todoList.push({
-                id : self.uuid(),
-                todo : todo,
-                completed : false
-              });
-              self.todo = ''
-    	  	};
-    	  }
+              // Function Declarations
 
-    	  self.toggleTodo = function (todo) {
-    	  	todo.completed = !todo.completed;
-    	  }
+              // create a unique id for the todo
+              function uuid() {
+                /*jshint bitwise:false */
+                var i, random;
+                var uuid = '';
 
-    	  self.toggleAll = function (checked) {
-    	  	self.todoList.map(function (todo) {
-    	  		todo.completed = checked;
-    	  	});
-    	  }
+                for (i = 0; i < 32; i++) {
+                    random = Math.random() * 16 | 0;
+                    if (i === 8 || i === 12 || i === 16 || i === 20) {
+                        uuid += '-';
+                    }
+                    uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
+                }
 
-    	  self.editTodo = function (todo, todoText) {
-    	  	var id = todo.id;
-    	  	if (todoText) {
-    	  	  self.todoList.map(function (todo) {
-    	  	    if(id === todo.id) {
-    	  	  	  todo.todo = todoText;
-    	  	    }
-    	  	  });
-    	  	};
-    	  }
+                return uuid;
+              } 
 
-    	  self.deleteTodo = function (todo, id) {
-    	  	self.todoList = self.todoList.filter(function (todo) {
-    	  		return !(id === todo.id)
-    	  	});
-    	  }
+              function addTodo(todo) { 
+                if(todo) {
+                  self.todoList.push({
+                    id : self.uuid(),
+                    todo : todo,
+                    completed : false
+                  });
+                  self.todo = ''
+                };
+              }
 
-    	  self.clearCompleted = function () {
-    	  	self.todoList = self.todoList.filter(function (todo) {
-    	  		return !todo.completed;
-    	  	});
-    	  }
+              function toggleTodo(todo) {
+                todo.completed = !todo.completed;
+              }
 
-    	  // responsible for all,active and completed tabs.
-    	  self.tab = 1;
+              function toggleAll(checked) {
+                self.todoList.map(function (todo) {
+                    todo.completed = checked;
+                });
+              }
 
-    	  self.tabIsSet = function (tab) {
-    	  	return self.tab === tab;
-    	  }
+              function editTodo(todo, todoText) {
+                var id = todo.id;
+                if (todoText) {
+                  self.todoList.map(function (todo) {
+                    if(id === todo.id) {
+                      todo.todo = todoText;
+                    }
+                  });
+                };
+              }
 
-    	  self.setTab = function (value) {
-    	  	self.tab = value;
-    	  }
-    	}
-    });
+              function deleteTodo(todo, id) {
+                self.todoList = self.todoList.filter(function (todo) {
+                    return !(id === todo.id)
+                });
+              }
+
+              function clearCompleted() {
+                self.todoList = self.todoList.filter(function (todo) {
+                    return !todo.completed;
+                });
+              }
+
+              // responsible for all,active and completed tabs.
+
+              function tabIsSet(tab) {
+                return self.tab === tab;
+              }
+
+              function setTab(value) {
+                self.tab = value;
+              }
+        }
+})();
